c4f072f71cd24ee392fd9fdc93eba07f74bbf6e4
Sakshi Antala <sakshi@saasinnova.com>
2025-04-02 14:02:30 +0530

    set classnames logic in useMemo

--- TextArea.tsx
+++ TextArea.tsx
@@ -4,6 +4,7 @@ import { inputValidator } from "../../../../library/utilities/helperFunction";
 import { IFormFieldType } from "../../../../library/utilities/constant";
 import { IFormProps } from "../formInterface/forms.model";
 import { FormFieldError } from "../formFieldError/FormFieldError";
+import { useMemo } from "react";
 
 export const TextArea = (props: IFormProps) => {
   const { attribute, form, fieldType } = props;
@@ -15,32 +16,23 @@ export const TextArea = (props: IFormProps) => {
     formState: { errors },
   } = useFormContext();
 
-  const getClassNames = () => {
-    let labelClassName = "";
-    let fieldClassName = "";
-    let divClassName = "";
-
+  const { labelClassName, fieldClassName, divClassName } = useMemo(() => {
     switch (fieldType) {
       case IFormFieldType.NO_LABEL:
-        labelClassName = "";
-        fieldClassName = "field p-fluid";
-        divClassName = "";
-        break;
       case IFormFieldType.TOP_LABEL:
-        labelClassName = "";
-        fieldClassName = "field p-fluid";
-        divClassName = "";
-        break;
+        return {
+          labelClassName: "",
+          fieldClassName: "field p-fluid",
+          divClassName: "",
+        };
       default:
-        labelClassName = "col-12 mb-3 md:col-3 md:mb-0";
-        fieldClassName = "field grid";
-        divClassName = "col-12 md:col-9 relative";
-        break;
+        return {
+          labelClassName: "col-12 mb-3 md:col-3 md:mb-0",
+          fieldClassName: "field grid",
+          divClassName: "col-12 md:col-9 relative",
+        };
     }
-
-    return { labelClassName, fieldClassName, divClassName };
-  };
-  const { labelClassName, fieldClassName, divClassName } = getClassNames();
+  }, [fieldType]);
 
   const labelElement = (
     <label htmlFor={attribute} className={labelClassName}>
